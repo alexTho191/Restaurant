bad71914004cb2e8d779c64c8fd6b9d9cf934c30
alexTho191 <alxndr.11deas@gmail.com>
2025-10-03 00:22:01 -0500

    ahora si

--- src/api/usuarios.jsx
+++ src/api/usuarios.jsx
@@ -1,6 +1,8 @@
-const API_URL = import.meta.env.PROD 
-  ? import.meta.env.VITE_API_URL 
-  : "/api";
+// Detecta si estamos en producción (Vercel) o en desarrollo (localhost)
+const API_URL =
+  import.meta.env.MODE === "development"
+    ? "/api" // usa el proxy local de vite
+    : import.meta.env.VITE_API_URL; // usa la URL real en Vercel
 
 export async function fetchUsuarios() {
   const res = await fetch(`${API_URL}/usuarios`);
--- vite.config.js
+++ vite.config.js
@@ -2,13 +2,12 @@ import { defineConfig } from 'vite'
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: import.meta.env.VITE_API_URL, // se usa solo en desarrollo
+        target: 'http://192.168.0.105:5000', // Cambiado a tu servidor local
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, '')
       }
